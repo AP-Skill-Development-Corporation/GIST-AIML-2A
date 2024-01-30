09808c5e63384c13a1b5783b62fb737b6195db97
GISTAIMLA <rravikumar34@gmail.com>
2024-01-30 17:13:17 +0530

    Day-02 Updated

---
+++ Day-02/Demo/college.py
@@ -0,0 +1,6 @@
+z='sai rohith'
+
+def dep():
+    print("Collge Departments")
+    print("1.CSE\n2.AIML\n3.EEE\n4.ECE")
+    return
---
+++ Day-02/sample.py
@@ -0,0 +1,11 @@
+g = "Sample User"
+
+def student():
+    '''
+    GIST Student Data
+    '''
+    n = input("Enter Student Name: ")
+    m = input("Student Id: ")
+    print(f"Student Name: {n}")
+    print(f"Student Id: {m}")
+    return
